ae705325c38103a5704da5c57e93707862982a30
ajaythopate <ajaythopate999@gmail.com>
2023-07-11 13:56:16 +0530

    js file html element added

---
+++ index.js
@@ -0,0 +1,21 @@
+// let data;
+// let forceCastOutput;
+
+
+const inputeBox = document.getElementById("InputBox");
+const headTime = document.getElementById("current-time");
+const countryName = document.getElementById("country-name");
+const cityName = document.getElementById("city-name");
+const stateName = document.getElementById("state-name");
+const dateTime = document.getElementById("timeDate");
+const logoImage = document.getElementById("logoImage");
+const humidity = document.getElementById("humidity");
+const windSpeed = document.getElementById("wind");
+const rain = document.getElementById("rain");
+const temprature = document.getElementById("current-temp");
+const wetherstatus = document.getElementById("wether-condition");
+const highTemp = document.getElementById("high-temp");
+const lowTemp = document.getElementById("low-temp");
+const sunRise = document.getElementById("sunrise");
+const sunset = document.getElementById("sunset");
+
